f510cb9bafab992ec509ce8782ef5439d58da17b
DiNyzier <wiritphon.tomo@bumail.net>
2025-03-10 21:14:03 +0700

    add gravity script

--- Assets/Scripts/GravityB.cs
+++ Assets/Scripts/GravityB.cs
@@ -1,16 +1,41 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityB : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody rb;
+    private const float G = 0.006674f;
+    public static List<GravityB> planetLists;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        if (planetLists == null)
+        {
+            planetLists = new List<GravityB>();
+        }
         
+        planetLists.Add(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        foreach (var planet in planetLists)
+        {
+            if (planet != this)
+                Attract(planet);
+        }
+    }
+
+    void Attract(GravityB other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
         
+        otherRb.AddForce(finalForce);
     }
 }
